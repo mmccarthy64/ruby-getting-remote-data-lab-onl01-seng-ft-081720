bac6600992e83e4d10965a74594f440520e0e361
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-09 19:46:42 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,16 @@
 # Write your code here
+class GetRequester
+  
+  def initialize
+    uri = URI.parse(URL)
+    
+  end
+  
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+    
+  end
+  
+end
